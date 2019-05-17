b3b3e8df4ff8af3a41c668db75eaefd79d205e44
Yasuto Tanaka <yasuto@ymail.com>
2019-05-17 01:35:39 -0300

    Tests: Add parenthesis balance test

--- tests/process_test.py
+++ tests/process_test.py
@@ -10,3 +10,11 @@ class CalculatorTest(TestCase):
     def test_remove_calculatorpy(self):
         result = process.remove_calculatorpy(['123', 'a', 'l', 'calculator.py', 'ldf'])
         self.assertEqual(result, ['123', 'a', 'l', '*', 'ldf'])
+
+    def test_are_parenthesis_balanced(self):
+        result = process.test_are_parenthesis_balanced(["(", "(", ")", "(", ")"])
+        self.assertEqual(result, False)
+    
+    def test_are_parenthesis_balanced_again(self):
+        result = process.test_are_parenthesis_balanced(["(", "(", ")", "(", "2", "3" ")", ")"])
+        self.assertEqual(result, True)
