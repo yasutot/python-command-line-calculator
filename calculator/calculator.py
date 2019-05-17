1fe4f4c57948d9f38fbce5ca6f21da9d8288493c
Yasuto Tanaka <yasuto@ymail.com>
2019-05-17 00:12:11 -0300

    Feat: Adds subtraction function

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,2 +1,5 @@
 def sum(num1, num2):
     return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -10,3 +10,15 @@ class CalculatorTest(TestCase):
     def test_another_sum(self):
         result = calculator.sum(10, 23)
         self.assertEqual(result, 33)
+    
+    def test_sum_negative_numbers(self):
+        result = calculator.sum(-3, -18)
+        self.assertEqual(result, -21)
+
+    def test_subtract(self):
+        result = calculator.subtract(5,3)
+        self.assertEqual(result, 2)
+
+    def test_subtract_negative_numbers(self):
+        result = calculator.subtract(-10, -2)
+        self.assertEqual(result, -8)
