47eea20bf8ccb2adbade39fd03c7f275234e50ca
Yasuto Tanaka <yasuto@ymail.com>
2019-05-17 01:26:50 -0300

    Style: Changes expression validation to expression processing

---
+++ tests/process_test.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from expression_processing import process
+
+class CalculatorTest(TestCase):
+
+    def test_get_expression_arguments(self):
+        result = process.get_expression_arguments(["a", "b", "c"])
+        self.assertEqual(result, ["b", "c"])
+
+    def test_remove_calculatorpy(self):
+        result = process.remove_calculatorpy(['123', 'a', 'l', 'calculator.py', 'ldf'])
+        self.assertEqual(result, ['123', 'a', 'l', '*', 'ldf'])
