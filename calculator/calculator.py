d30899c8a12bf0b7aa84413a897fbdd8363c74df
Yasuto Tanaka <yasuto@ymail.com>
2019-05-17 00:31:34 -0300

    Feat: Adds division function

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -6,3 +6,9 @@ def subtract(num1, num2):
 
 def multiply(num1, num2):
     return num1 * num2
+
+def divide(num1, num2):
+    if (num2 == 0):
+        raise Exception("Cannot divide by zero")
+
+    return num1 // num2
